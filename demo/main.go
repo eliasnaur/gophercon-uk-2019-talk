3f4b0aa93f92d826276ed61b8201a6abf5377010
Elias Naur <mail@eliasnaur.com>
2019-08-22 17:23:45 +0200

    demo: IconButton

--- demo/main.go
+++ demo/main.go
@@ -1,16 +1,19 @@
 package main
 
-import "gioui.org/ui/app"
-import "gioui.org/ui"
-import "gioui.org/ui/layout"
 import "gophercon/simple"
+
+import "gioui.org/ui"
+import "gioui.org/ui/app"
+import "gioui.org/ui/layout"
+import "log"
 
 func main() {
 	go func() {
 		theme := simple.NewTheme()
 		w := app.NewWindow()
 		ops := new(ui.Ops)
-		editor := theme.Editor(46)
+		btn := new(simple.IconButton)
+		q := w.Queue()
 		for e := range w.Events() {
 			switch e := e.(type) {
 			case app.UpdateEvent:
@@ -18,7 +21,10 @@ func main() {
 				ops.Reset()
 				theme.Reset(cfg)
 				cs := layout.RigidConstraints(e.Size)
-				editor.Layout(cfg, w.Queue(), ops, cs)
+				for e, ok := btn.Next(q); ok; e, ok = btn.Next(q) {
+					log.Println(e)
+				}
+				btn.Layout(cfg, ops, cs)
 				w.Update(ops)
 			}
 		}
