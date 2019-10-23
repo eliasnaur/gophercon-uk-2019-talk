fa303f50c5abe0e02c7e8a270a939a7b959e8e7e
Elias Naur <mail@eliasnaur.com>
2019-10-23 10:38:56 +0200

    update gio version

--- demo/main.go
+++ demo/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	"gioui.org/app"
-	_ "gioui.org/font/gofont"
+	"gioui.org/font/gofont"
 	"gioui.org/io/system"
 	"gioui.org/layout"
 	"gioui.org/unit"
@@ -15,6 +15,7 @@ import (
 
 func main() {
 	go func() {
+		gofont.Register()
 		theme := material.NewTheme()
 
 		ico, _ := material.NewIcon(icons.ContentAdd)
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/eliasnaur/gophercon-uk-2019-talk
 go 1.13
 
 require (
-	gioui.org v0.0.0-20191014162007-f5e93206f602
+	gioui.org v0.0.0-20191023082108-9a62468c25c4
 	golang.org/x/exp v0.0.0-20191002040644-a1355ae1e2c3
 )
